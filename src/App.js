310d00b67914cedeee86e8b5747bd5a7a3dc4c1d
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2021-05-17 01:10:53 +0530

    warning removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import HomeScreen from "./HomeScreen"
-import Nav from "./Nav"
+import React from "react";
+// import { Counter } from './features/counter/Counter';
+import "./App.css";
+import HomeScreen from "./HomeScreen";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
-     <HomeScreen />
-     <Nav />
+      <HomeScreen />
+      <Nav />
     </div>
   );
 }
