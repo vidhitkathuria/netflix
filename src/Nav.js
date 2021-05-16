b674f05939031137842e26ab261cbcd582202b4c
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2021-05-17 01:03:06 +0530

    Frontend (Day 2)

---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav_content">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+          alt=""
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlo_BNAP4g2P4_TC1w46AsxMVHw8fRX5A",
+    authDomain: "netflix-clone-527d6.firebaseapp.com",
+    projectId: "netflix-clone-527d6",
+    storageBucket: "netflix-clone-527d6.appspot.com",
+    messagingSenderId: "294586872169",
+    appId: "1:294586872169:web:41aab55ab8a2e5157e4df8"
+  };
